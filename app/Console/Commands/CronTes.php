3d8fb00d4e6449b18d1b6e65086d916f0595269d
bayudiarta <bayudiarta230@gmail.com>
2022-04-07 14:03:23 +0700

    make it better

--- app/Console/Commands/CronTes.php
+++ app/Console/Commands/CronTes.php
@@ -2,24 +2,24 @@
 
 namespace App\Console\Commands;
 
-use Carbon\Carbon;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
 
-class DatabaseBackUp extends Command
+class CronTes extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'database:backup';
+    protected $signature = 'cron:log';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Backup database';
+    protected $description = 'Log Backup database';
 
     /**
      * Create a new command instance.
@@ -38,13 +38,6 @@ class DatabaseBackUp extends Command
      */
     public function handle()
     {
-        $filename = "backup-" . Carbon::now()->format('Y-m-d') . ".sql";
-
-        $command = "mysqldump --user=" . env('DB_USERNAME') . " --password=" . env('DB_PASSWORD') . " --host=" . env('DB_HOST') . " " . env('DB_DATABASE') . " > " . storage_path() . "/app/backup/" . $filename;
-
-        $returnVar = NULL;
-        $output  = NULL;
-
-        exec($command, $output, $returnVar);
+        Log::info("Cron is working fine!"); 
     }
 }
