1138a4ef9fe295c050e6eb9fef9b2a0f4a373fc0
bayudiarta <bayudiarta230@gmail.com>
2022-04-07 16:04:03 +0700

    laste updatededededfddede

--- database/migrations/2022_03_29_035028_create_log_aktifitas_table.php
+++ database/migrations/2022_03_29_035028_create_log_aktifitas_table.php
@@ -37,6 +37,7 @@ class CreateLogAktifitasTable extends Migration
             RETURNS trigger AS $$
             BEGIN
                 INSERT log_aktifitas(users_id,aksi, waktu,tipe) VALUES (old.users_id, CONCAT(menghapus Catatan Di lokasi:, old.lokasi),now(),3 )
+                RETURN log_aktifitas;
             END
             $$ LANGUAGE plpgsql;
             
