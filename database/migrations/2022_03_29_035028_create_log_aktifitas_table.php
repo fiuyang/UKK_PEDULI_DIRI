8648a9618cee21f489d99576e397464a042dee14
bayudiarta <bayudiarta230@gmail.com>
2022-04-07 15:57:31 +0700

    laste updatededededfdded

--- database/migrations/2022_03_29_035028_create_log_aktifitas_table.php
+++ database/migrations/2022_03_29_035028_create_log_aktifitas_table.php
@@ -36,8 +36,7 @@ class CreateLogAktifitasTable extends Migration
             CREATE OR REPLACE FUNCTION thapus_perjalanan()
             RETURNS trigger AS $$
             BEGIN
-                INSERT log_aktifitas(users_id,aksi, waktu,tipe) VALUES (old.users_id, CONCAT(menghapus Catatan Di lokasi:, old.lokasi),now(),3 );
-                RETURN NULL;
+                'INSERT log_aktifitas(users_id,aksi, waktu,tipe) VALUES (old.users_id, CONCAT(menghapus Catatan Di lokasi:, old.lokasi),now(),3 )';
             END
             $$ LANGUAGE plpgsql;
             
