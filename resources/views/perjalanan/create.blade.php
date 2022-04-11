c634b73e1effb5d61ac38db20fd13b57c360766d
bayudiarta <bayudiarta230@gmail.com>
2022-04-11 15:55:04 +0700

    solving gis

--- resources/views/perjalanan/create.blade.php
+++ resources/views/perjalanan/create.blade.php
@@ -6,15 +6,15 @@
         <div class="card">
             <div class="card-body">
                 @if(session('success'))
-                    <div class="alert alert-success alert-dismissible fade show" role="alert">
-                        <span class="alert-inner--icon"><i class="ni ni-like-2"></i></span>
-                        <span class="alert-inner--text">
-                            {{ session('success') }}
-                        </span>
-                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                    <span class="alert-inner--icon"><i class="ni ni-like-2"></i></span>
+                    <span class="alert-inner--text">
+                        {{ session('success') }}
+                    </span>
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
                 @endif
                 @if($errors->any())
                 <div class="alert alert-danger" role="alert">
@@ -84,8 +84,8 @@
 @endsection
 @section('styles')
 <link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.1/dist/leaflet.css"
-    integrity="sha512-Rksm5RenBEKSKFjgI3a41vrjkw4EVPlJ3+OiI65vTjIdo9brlAacEuKOiQ5OFh7cOI1bkDwLqdLw3Zg0cRJAAQ=="
-    crossorigin=""/>
+integrity="sha512-Rksm5RenBEKSKFjgI3a41vrjkw4EVPlJ3+OiI65vTjIdo9brlAacEuKOiQ5OFh7cOI1bkDwLqdLw3Zg0cRJAAQ=="
+crossorigin=""/>
 
 <style>
     #mapid { height: 500px; }
@@ -94,8 +94,8 @@
 
 @section('script')
 <script src="https://unpkg.com/leaflet@1.3.1/dist/leaflet.js"
-    integrity="sha512-/Nsx9X4HebavoBvEBuyp3I7od5tA0UzAxs+j83KgC8PU0kgB4XiK4Lfe4y4cgBtaRJQEIFCW+oC506aPT2L1zw=="
-    crossorigin=""></script>
+integrity="sha512-/Nsx9X4HebavoBvEBuyp3I7od5tA0UzAxs+j83KgC8PU0kgB4XiK4Lfe4y4cgBtaRJQEIFCW+oC506aPT2L1zw=="
+crossorigin=""></script>
 <script src="https://unpkg.com/leaflet-control-geocoder/dist/Control.Geocoder.js"></script>
 <script>
     $(document).ready(function () {
@@ -107,52 +107,97 @@
             }
         });
     });
+
+    var latlng = [{{ request('latitude', config('leaflet.map_center_latitude')) }}, {{ request('longitude', config('leaflet.map_center_longitude')) }}];
+    var map_init = L.map('mapid').setView(latlng, {{ config('leaflet.zoom_level') }});
     
-
-    // var latlng = [{{ request('latitude', config('leaflet.map_center_latitude')) }}, {{ request('longitude', config('leaflet.map_center_longitude')) }}];
-    // var map = L.map('mapid').setView(latlng, {{ config('leaflet.zoom_level') }});
     
-    navigator.geolocation.getCurrentPosition(function(location) {
-    var latlng = new L.LatLng(location.coords.latitude, location.coords.longitude);
-
-    var map = L.map('mapid').setView(latlng, {{ config('leaflet.zoom_level') }})
-
-    L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
-        maxZoom: 20,
-        subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
-    }).addTo(map);
-    L.Control.geocoder().addTo(map);
-
+    // var map_init = L.map('mapid', {
+    //     center: [9.0820, 8.6753],
+    //     zoom: 8
+    // });
+    var osm = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+                maxZoom: 20,
+                subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+            }).addTo(map_init);
+    L.Control.geocoder().addTo(map_init);
     if (!navigator.geolocation) {
-        swal.fire("Error!", 'Browser Kamu Tidak Mendukung Geolocation Sekarang.', "error");
-        // console.log("Your browser doesn't support geolocation feature!")
+        console.log("Your browser doesn't support geolocation feature!")
     } else {
         setInterval(() => {
             navigator.geolocation.getCurrentPosition(getPosition)
         }, 5000);
     };
-    var marker = L.marker(latlng).addTo(map);
-    function updateMarker(lat, lng) {
-        marker
-        .setLatLng([lat, lng])
-        .bindPopup("Your location :  " + marker.getLatLng().toString())
-        .openPopup();
-        return false;
-    };
-
-    map.on('click', function(e) {
-        let latitude = e.latlng.lat.toString().substring(0, 15);
-        let longitude = e.latlng.lng.toString().substring(0, 15);
-        $('#latitude').val(latitude);
-        $('#longitude').val(longitude);
-        updateMarker(latitude, longitude);
-    });
-
-    var updateMarkerByInputs = function() {
-        return updateMarker( $('#latitude').val() , $('#longitude').val());
+    var marker, circle, lat, long, accuracy;
+    
+    function getPosition(position) {
+        // console.log(position)
+        lat = position.coords.latitude
+        long = position.coords.longitude
+        accuracy = position.coords.accuracy
+        
+        if (marker) {
+            map_init.removeLayer(marker)
+        }
+        
+        if (circle) {
+            map_init.removeLayer(circle)
+        }
+        
+        marker = L.marker([lat, long])
+        circle = L.circle([lat, long], { radius: accuracy })
+        
+        var featureGroup = L.featureGroup([marker, circle]).addTo(map_init)
+        
+        map_init.fitBounds(featureGroup.getBounds())
+        
+        console.log("Your coordinate is: Lat: " + lat + " Long: " + long + " Accuracy: " + accuracy)
     }
-    $('#latitude').on('input', updateMarkerByInputs);
-    $('#longitude').on('input', updateMarkerByInputs);
-    });
-</script>    
-@endsection
+    
+    // var latlng = [{{ request('latitude', config('leaflet.map_center_latitude')) }}, {{ request('longitude', config('leaflet.map_center_longitude')) }}];
+    // var map = L.map('mapid').setView(latlng, {{ config('leaflet.zoom_level') }});
+    
+    // navigator.geolocation.getCurrentPosition(function(location) {
+        // var latlng = new L.LatLng(location.coords.latitude, location.coords.longitude);
+        
+        // var map = L.map('mapid').setView(latlng, {{ config('leaflet.zoom_level') }})
+        
+        // L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+            //     maxZoom: 20,
+            //     subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+            // }).addTo(map);
+            // L.Control.geocoder().addTo(map);
+            
+            // if (!navigator.geolocation) {
+                //     swal.fire("Error!", 'Browser Kamu Tidak Mendukung Geolocation Sekarang.', "error");
+                //     // console.log("Your browser doesn't support geolocation feature!")
+                // } else {
+                    //     setInterval(() => {
+                        //         navigator.geolocation.getCurrentPosition(getPosition)
+                        //     }, 5000);
+                        // };
+                        // var marker = L.marker(latlng).addTo(map);
+                        // function updateMarker(lat, lng) {
+                            //     marker
+                            //     .setLatLng([lat, lng])
+                            //     .bindPopup("Your location :  " + marker.getLatLng().toString())
+                            //     .openPopup();
+                            //     return false;
+                            // };
+                            
+                            // map.on('click', function(e) {
+                                //     let latitude = e.latlng.lat.toString().substring(0, 15);
+                                //     let longitude = e.latlng.lng.toString().substring(0, 15);
+                                //     $('#latitude').val(latitude);
+                                //     $('#longitude').val(longitude);
+                                //     updateMarker(latitude, longitude);
+                                // });
+                                
+                                // var updateMarkerByInputs = function() {
+                                    //     return updateMarker( $('#latitude').val() , $('#longitude').val());
+                                    // }
+                                    // $('#latitude').on('input', updateMarkerByInputs);
+                                    // $('#longitude').on('input', updateMarkerByInputs);
+                                    // });
+                                </script>    
+                                @endsection
