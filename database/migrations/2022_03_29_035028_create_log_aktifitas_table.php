ddc8bc84b3767ddcf5fa6eccc8f9e8b499ca36c0
bayudiarta <bayudiarta230@gmail.com>
2022-04-13 23:25:55 +0700

    anjas

--- database/migrations/2022_03_29_035028_create_log_aktifitas_table.php
+++ database/migrations/2022_03_29_035028_create_log_aktifitas_table.php
@@ -58,7 +58,7 @@ class CreateLogAktifitasTable extends Migration
             CREATE OR REPLACE FUNCTION tcreate_perjalanan()
             RETURNS trigger AS $$
             BEGIN
-                INSERT INTO log_aktifitas(users_id,aktifitas,waktu,tipe) SELECT (NEW.users_id, NEW.lokasi, now(), 1);
+                INSERT INTO log_aktifitas(users_id,aktifitas,waktu,tipe) VALUES (NEW.users_id, NEW.lokasi, now(), 1);
                 RETURN null;
             END
             $$ LANGUAGE plpgsql;
